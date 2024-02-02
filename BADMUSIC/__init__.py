56aa2ab4476bd20a9e7888cc19ec03fbc9aec2a6
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2024-02-03 00:34:04 +0530

    Create __init__.py

---
+++ BADMUSIC/__init__.py
@@ -0,0 +1,28 @@
+from BADMUSIC.core.bot import VIP
+from BADMUSIC.core.dir import dirr
+from BADMUSIC.core.git import git
+from BADMUSIC.core.userbot import Userbot
+from BADMUSIC.misc import dbb, heroku
+
+from SafoneAPI import SafoneAPI
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = BAD()
+api = SafoneAPI()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
