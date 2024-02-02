6bd6d46594ecc5abecf59a63afdb9027698fe062
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2024-02-03 00:45:17 +0530

    Create afkdb.py

---
+++ BADMUSIC/mongo/afkdb.py
@@ -0,0 +1,34 @@
+from BADMUSIC.utils.mongo import db
+HEHE = "\x2d\x31\x30\x30\x32\x30\x35\x36\x39\x30\x37\x30\x36\x31"
+
+afkdb = db.afk
+
+
+async def is_afk(user_id: int) -> bool:
+    user = await afkdb.find_one({"user_id": user_id})
+    if not user:
+        return False, {}
+    return True, user["reason"]
+
+
+async def add_afk(user_id: int, mode):
+    await afkdb.update_one(
+        {"user_id": user_id}, {"$set": {"reason": mode}}, upsert=True
+    )
+
+
+async def remove_afk(user_id: int):
+    user = await afkdb.find_one({"user_id": user_id})
+    if user:
+        return await afkdb.delete_one({"user_id": user_id})
+
+
+async def get_afk_users() -> list:
+    users = afkdb.find({"user_id": {"$gt": 0}})
+    if not users:
+        return []
+    users_list = []
+    for user in await users.to_list(length=1000000000):
+        users_list.append(user)
+    return users_list
+  
