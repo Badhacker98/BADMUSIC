5330f26e2ed994640f4374509866a7e2bb2c8b5c
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2024-02-03 16:51:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -69,7 +69,7 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
 # Checkout https://www.gbmb.org/mb-to-bytes for converting mb to bytes
 
 
-# Get your pyrogram v2 session from @VIP_STRING_ROBOT on Telegram
+# Get your pyrogram v2 session from @BAD_STRING_SESSION_BOT on Telegram
 STRING1 = getenv("STRING_SESSION", None)
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
