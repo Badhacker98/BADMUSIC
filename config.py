eecaa2a79bcea0a2e6ee450fe2e7817c2f7c72cf
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2024-02-03 15:51:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/badmunda98/testbot",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "BAD")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
