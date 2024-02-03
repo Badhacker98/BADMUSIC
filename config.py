f8156897aae05a8ddd0f0fb8b807eb4217cae0e0
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2024-02-03 15:33:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/badmunda98/BADOP",
+    "https://github.com/MXNIHACKER/BrokenMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
