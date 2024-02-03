89f78b69cae1a9b196ae44759f3b3ad958a0edc2
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2024-02-03 12:54:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/MXNIHACKER/PbxMusic",
+    "https://github.com/badmunda98/nikllode",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
