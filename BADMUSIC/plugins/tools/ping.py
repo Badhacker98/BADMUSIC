55ebdd4d938213d6f538074d595aa28a48fdab3e
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-05-07 08:12:40 +0530

    Update ping.py

--- BADMUSIC/plugins/tools/ping.py
+++ BADMUSIC/plugins/tools/ping.py
@@ -24,7 +24,7 @@ async def make_carbon(code):
     image.name = "carbon.png"
     return image
 
-@app.on_message(filters.command("ping", prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~BANNED_USERS)
+@app.on_message(filters.command("ping", prefixes=["/"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
     PING_IMG_URL = "https://telegra.ph/file/7bb907999ea7156227283.jpg"
