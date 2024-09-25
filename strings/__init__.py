3a3d4c2e832020c5a3216e627ba1b94a7ef915d1
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-09-25 23:31:40 +0530

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -1,14 +1,32 @@
+
+# All rights reserved
+
 import os
+import sys
 from typing import List
 
 import yaml
 
 languages = {}
+commands = {}
+
 languages_present = {}
+
+
+def get_command(value: str) -> List:
+    return commands["command"][value]
 
 
 def get_string(lang: str):
     return languages[lang]
+
+
+for filename in os.listdir(r"./strings"):
+    if filename.endswith(".yml"):
+        language_name = filename[:-4]
+        commands[language_name] = yaml.safe_load(
+            open(r"./strings/" + filename, encoding="utf8")
+        )
 
 
 for filename in os.listdir(r"./strings/langs/"):
@@ -30,6 +48,8 @@ for filename in os.listdir(r"./strings/langs/"):
     try:
         languages_present[language_name] = languages[language_name]["name"]
     except:
-        print("There is some issue with the language file inside bot.")
-        exit()
+        print(
+            "There is some issue with the language file inside bot. Please report it to the @PBX_CHAT on Telegram"
+        )
+        sys.exit()
         
