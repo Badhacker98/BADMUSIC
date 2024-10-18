ceb20e6a87e711feda48ce64130ac77ddf40c70c
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-10-18 11:14:38 +0530

    Update stream.py

--- BADMUSIC/utils/stream/stream.py
+++ BADMUSIC/utils/stream/stream.py
@@ -8,6 +8,7 @@ from typing import Union
 from pyrogram.types import InlineKeyboardMarkup
 
 import config
+from config import LOG_GROUP_ID, OWNER_ID
 from BADMUSIC import Carbon, YouTube, app
 from BADMUSIC.core.call import BAD
 from BADMUSIC.misc import db
@@ -89,7 +90,15 @@ async def stream(
                         vidid, mystic, video=status, videoid=True
                     )
                 except:
-                    return await mystic.delete()
+                    await mystic.delete()
+                    await app.send_message(
+                        LOG_GROUP_ID,
+                        f"**ʜᴇʏ [ᴏᴡɴᴇʀ](tg://user?id={OWNER_ID[0]}) ᴍᴀʏ ʙᴇ ᴍʏ ᴄᴏᴏᴋɪᴇs ʜᴀs ʙᴇᴇɴ ᴅᴇᴀᴅ ᴘʟᴇᴀsᴇ ᴄʜᴇᴄᴋ ᴏɴᴇ ᴛɪᴍᴇ ʙʏ ᴘʟᴀʏ ᴀɴʏ sᴏɴɢs**",
+                    )
+                    return await app.send_message(
+                        OWNER_ID[0],
+                        f"**ʜᴇʏ [ᴏᴡɴᴇʀ](tg://user?id={OWNER_ID[0]}) ᴍᴀʏ ʙᴇ ᴍʏ ᴄᴏᴏᴋɪᴇs ʜᴀs ʙᴇᴇɴ ᴅᴇᴀᴅ ᴘʟᴇᴀsᴇ ᴄʜᴇᴄᴋ ᴏɴᴇ ᴛɪᴍᴇ ʙʏ ᴘʟᴀʏ ᴀɴʏ sᴏɴɢs**",
+                    )
 
                 await BAD.join_call(
                     chat_id, original_chat_id, file_path, video=status, image=thumbnail
@@ -150,7 +159,15 @@ async def stream(
                 vidid, mystic, videoid=True, video=status
             )
         except:
-            return await mystic.delete()
+            await mystic.delete()
+            await app.send_message(
+                LOG_GROUP_ID,
+                f"**ʜᴇʏ [ᴏᴡɴᴇʀ](tg://user?id={OWNER_ID[0]}) ᴍᴀʏ ʙᴇ ᴍʏ ᴄᴏᴏᴋɪᴇs ʜᴀs ʙᴇᴇɴ ᴅᴇᴀᴅ ᴘʟᴇᴀsᴇ ᴄʜᴇᴄᴋ ᴏɴᴇ ᴛɪᴍᴇ ʙʏ ᴘʟᴀʏ ᴀɴʏ sᴏɴɢs**",
+            )
+            return await app.send_message(
+                OWNER_ID[0],
+                f"**ʜᴇʏ [ᴏᴡɴᴇʀ](tg://user?id={OWNER_ID[0]}) ᴍᴀʏ ʙᴇ ᴍʏ ᴄᴏᴏᴋɪᴇs ʜᴀs ʙᴇᴇɴ ᴅᴇᴀᴅ ᴘʟᴇᴀsᴇ ᴄʜᴇᴄᴋ ᴏɴᴇ ᴛɪᴍᴇ ʙʏ ᴘʟᴀʏ ᴀɴʏ sᴏɴɢs**",
+            )
 
         if await is_active_chat(chat_id):
             await put_queue(
@@ -422,4 +439,4 @@ async def stream(
             db[chat_id][0]["mystic"] = run
             db[chat_id][0]["markup"] = "tg"
             await mystic.delete()
-                
+            
