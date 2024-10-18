39e4d6688c24a1eaebc6128bed01803a6cf31008
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-10-18 17:40:41 +0530

    Create clone.py

---
+++ BADMUSIC/plugins/tools/clone.py
@@ -0,0 +1,171 @@
+import re
+import logging
+import asyncio
+import importlib
+from sys import argv
+from pyrogram import idle
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors.exceptions.bad_request_400 import (
+    AccessTokenExpired,
+    AccessTokenInvalid,
+)
+from BADMUSIC.utils.database import get_assistant
+from config import API_ID, API_HASH
+from BADMUSIC import app
+from BADMUSIC.misc import SUDOERS
+from BADMUSIC.utils.database import get_assistant, clonebotdb
+from config import LOG_GROUP_ID 
+
+CLONES = set()
+
+
+@app.on_message(filters.command("clone2") & SUDOERS)
+async def clone_txt(client, message):
+    userbot = await get_assistant(message.chat.id)
+    if len(message.command) > 1:
+        bot_token = message.text.split("/clone", 1)[1].strip()
+        mi = await message.reply_text("ᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ ᴡʜɪʟᴇ ɪ ᴘʀᴏᴄᴇꜱꜱ ᴛʜᴇ ʙᴏᴛ ᴛᴏᴋᴇɴ 💫 ")
+        try:
+            ai = Client(
+                bot_token,
+                API_ID,
+                API_HASH,
+                bot_token=bot_token,
+                plugins=dict(root="BADMUSIC.cplugin"),
+            )
+            await ai.start()
+            bot = await ai.get_me()
+            bot_users = await ai.get_users(bot.username)
+            bot_id = bot_users.id
+
+        except (AccessTokenExpired, AccessTokenInvalid):
+            await mi.edit_text(
+                "ʏᴏᴜ ʜᴀᴠᴇ ᴘʀᴏᴠɪᴅᴇᴅ ᴀɴ ɪɴᴠᴀʟɪᴅ ʙᴏᴛ ᴛᴏᴋᴇɴ. ᴘʟᴇᴀꜱᴇ ᴘʀᴏᴠɪᴅᴇ ᴀ ᴠᴀʟɪᴅ ʙᴏᴛ ᴛᴏᴋᴇɴ."
+            )
+            return
+        except Exception as e:
+            await mi.edit_text(f"An error occurred: {str(e)}")
+            return
+
+        # Proceed with the cloning process
+        await mi.edit_text(
+            "ᴄʟᴏɴɪɴɢ ᴘʀᴏᴄᴇꜱꜱ ꜱᴛᴀʀᴛᴇᴅ. ᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ ꜰᴏʀ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ʙᴇ ꜱᴛᴀʀᴛ."
+        )
+        try:
+
+            await app.send_message(
+                LOG_GROUP_ID, f"**#New_Clones**\n\n**Bot:- @{bot.username}**"
+            )
+            await userbot.send_message(bot.username, "/start")
+
+            details = {
+                "bot_id": bot.id,
+                "is_bot": True,
+                "user_id": message.from_user.id,
+                "name": bot.first_name,
+                "token": bot_token,
+                "username": bot.username,
+            }
+            clonebotdb.insert_one(details)
+            CLONES.add(bot.id)
+            await mi.edit_text(
+                f"ʙᴏᴛ @{bot.username} ʜᴀꜱ ʙᴇᴇɴ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ ᴄʟᴏɴᴇᴅ ᴀɴᴅ ꜱᴛᴀʀᴛᴇᴅ ✅.\n**ʀᴇᴍᴏᴠᴇ ᴄʟᴏɴᴇᴅ ʙʏ :- /delclone**"
+            )
+        except BaseException as e:
+            logging.exception("Error while cloning bot.")
+            await mi.edit_text(
+                f"⚠️ <b>ᴇʀʀᴏʀ:</b>\n\n<code>{e}</code>\n\n**ᴋɪɴᴅʟʏ ғᴏᴡᴀʀᴅ ᴛʜɪs ᴍᴇssᴀɢᴇ ᴛᴏ @vk_zone ᴛᴏ ɢᴇᴛ ᴀssɪsᴛᴀɴᴄᴇ**"
+            )
+    else:
+        await message.reply_text(
+            "**ɢɪᴠᴇ ʙᴏᴛ ᴛᴏᴋᴇɴ ᴀꜰᴛᴇʀ /clone ᴄᴏᴍᴍᴀɴᴅ ꜰʀᴏᴍ @Botfather.**"
+        )
+
+
+@app.on_message(
+    filters.command(
+        [
+            "deletecloned",
+            "delcloned",
+            "delclone",
+            "deleteclone",
+            "removeclone",
+            "cancelclone",
+        ]
+    )
+)
+async def delete_cloned_bot(client, message):
+    try:
+        if len(message.command) < 2:
+            await message.reply_text(
+                "**⚠️ ᴘʟᴇᴀꜱᴇ ᴘʀᴏᴠɪᴅᴇ ᴛʜᴇ ʙᴏᴛ ᴛᴏᴋᴇɴ ᴀꜰᴛᴇʀ ᴛʜᴇ ᴄᴏᴍᴍᴀɴᴅ.**"
+            )
+            return
+
+        bot_token = " ".join(message.command[1:])
+        await message.reply_text("ᴘʀᴏᴄᴇꜱꜱɪɴɢ ᴛʜᴇ ʙᴏᴛ ᴛᴏᴋᴇɴ...")
+
+        cloned_bot = clonebotdb.find_one({"token": bot_token})
+        if cloned_bot:
+            clonebotdb.delete_one({"token": bot_token})
+            CLONES.remove(cloned_bot["bot_id"])
+            await message.reply_text(
+                "**🤖 ʏᴏᴜʀ ᴄʟᴏɴᴇᴅ ʙᴏᴛ ʜᴀꜱ ʙᴇᴇɴ ᴅɪꜱᴄᴏɴɴᴇᴄᴛᴇᴅ ꜰʀᴏᴍ ᴍʏ ꜱᴇʀᴠᴇʀ ☠️\nᴄʟᴏɴᴇ ʙʏ :- /clone**"
+            )
+        else:
+            await message.reply_text(
+                "**⚠️ ᴛʜᴇ ᴘʀᴏᴠɪᴅᴇᴅ ʙᴏᴛ ᴛᴏᴋᴇɴ ɪꜱ ɴᴏᴛ ɪɴ ᴛʜᴇ ᴄʟᴏɴᴇᴅ ʟɪꜱᴛ.**"
+            )
+    except Exception as e:
+        await message.reply_text("ᴀɴ ᴇʀʀᴏʀ ᴏᴄᴄᴜʀʀᴇᴅ ᴡʜɪʟᴇ ᴅᴇʟᴇᴛɪɴɢ ᴛʜᴇ ᴄʟᴏɴᴇᴅ ʙᴏᴛ.")
+        logging.exception(e)
+
+
+async def restart_bots():
+    global CLONES
+    try:
+        logging.info("ʀᴇꜱᴛᴀʀᴛɪɴɢ ᴀʟʟ ᴄʟᴏɴᴇᴅ ʙᴏᴛꜱ.....")
+        bots = clonebotdb.find()
+        async for bot in bots:
+            bot_token = bot["token"]
+            ai = Client(
+                f"{bot_token}",
+                API_ID,
+                API_HASH,
+                bot_token=bot_token,
+                plugins=dict(root="BADMUSIC.cplugin"),
+            )
+            await ai.start()
+            bot = await ai.get_me()
+            if bot.id not in CLONES:
+                try:
+                    CLONES.add(bot.id)
+                except Exception:
+                    pass
+    except Exception as e:
+        logging.exception("ᴇʀʀᴏʀ ᴡʜɪʟᴇ ʀᴇꜱᴛᴀʀᴛɪɴɢ ʙᴏᴛꜱ.")
+
+
+@app.on_message(filters.command("clones") & SUDOERS)
+async def list_cloned_bots(client, message):
+    try:
+        cloned_bots = clonebotdb.find()
+        cloned_bots_list = await cloned_bots.to_list(length=None)
+
+        if not cloned_bots_list:
+            await message.reply_text("ɴᴏ ʙᴏᴛꜱ ʜᴀᴠᴇ ʙᴇᴇɴ ᴄʟᴏɴᴇᴅ ʏᴇᴛ.")
+            return
+
+        total_clones = len(cloned_bots_list)
+        text = f"ᴛᴏᴛᴀʟ ᴄʟᴏɴᴇᴅ ʙᴏᴛs: {total_clones}\n\n"
+
+        for bot in cloned_bots_list:
+            text += f"Bot ID: {bot['bot_id']}\n"
+            text += f"Bot Name: {bot['name']}\n"
+            text += f"Bot Username: @{bot['username']}\n\n"
+
+        await message.reply_text(text)
+    except Exception as e:
+        logging.exception(e)
+        await message.reply_text("ᴀɴ ᴇʀʀᴏʀ ᴏᴄᴄᴜʀʀᴇᴅ ᴡʜɪʟᴇ ʟɪꜱᴛɪɴɢ ᴄʟᴏɴᴇᴅ ʙᴏᴛꜱ.")
