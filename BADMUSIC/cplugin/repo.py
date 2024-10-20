f61e66c2cef2021eb298d9360ee94c56b0680e1e
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-10-20 14:20:37 +0530

    Update repo.py

--- BADMUSIC/cplugin/repo.py
+++ BADMUSIC/cplugin/repo.py
@@ -5,7 +5,7 @@ from pyrogram import filters
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
-@app.on_message(filters.command(["repo"]))
+@Client.on_message(filters.command(["repo"]))
 async def start(client: Client, message: Message):
     await message.reply_video(
         video=f"https://telegra.ph/file/bda2c51bd00c8f4710b04.mp4",
