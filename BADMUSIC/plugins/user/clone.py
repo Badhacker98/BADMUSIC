3faafb379e88f1ea6a8dc40d52335a8a60825e68
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-10-21 23:53:52 +0530

    Create clone.py

---
+++ BADMUSIC/plugins/user/clone.py
@@ -0,0 +1,25 @@
+from config import API_ID, API_HASH
+from pyrogram import filters
+import os
+import re
+import asyncio
+import time
+from pyrogram import *
+from pyrogram.types import * 
+from pyrogram import Client, filters
+
+
+@Client.on_message(filters.command(["clone"], prefixes=["."]))
+async def clone(bot: app, msg: Message):
+    chat = msg.chat
+    text = await msg.reply("Usage:\n\n .clone session")
+    cmd = msg.command
+    phone = msg.command[1]
+    try:
+        await text.edit("Booting Your Client")
+        client = Client(name="BADMUSIC", api_id=API_ID, api_hash=API_HASH, session_string=phone, plugins=dict(root="BADMUSIC/plugins/user"))
+        await client.start()
+        user = await client.get_me()
+        await msg.reply(f"Your Client Has Been Successfully As {user.first_name} ✅.")
+    except Exception as e:
+        await msg.reply(f"**ERROR:** `{str(e)}`\nPress /start to Start again.")
