e50a44980e073a6180de6eadb4cd494671f8371d
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-10-22 18:22:17 +0530

    Update __init__.py

--- BADMUSIC/__init__.py
+++ BADMUSIC/__init__.py
@@ -1,6 +1,9 @@
 # Copyright (C) 2024 by Badhacker98@Github, < https://github.com/Badhacker98 >.
 # Owner https://t.me/ll_BAD_MUNDA_ll
 
+import json
+import os
+
 from BADMUSIC.core.bot import BADBOT
 from BADMUSIC.core.dir import dirr
 from BADMUSIC.core.git import git
