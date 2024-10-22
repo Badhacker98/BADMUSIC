7a27799478fac6af3c0ab734f64b9b1aa316e48c
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-10-22 08:19:22 +0530

    Create update.py

---
+++ BADMUSIC/plugins/user/update.py
@@ -0,0 +1,281 @@
+
+import asyncio
+import math
+import os
+import shutil
+import socket
+from datetime import datetime
+
+import dotenv
+import heroku3
+import requests
+import urllib3
+from git import Repo
+from git.exc import GitCommandError, InvalidGitRepositoryError
+from pyrogram import filters
+from pyrogram import filters, Client
+
+import config
+from config import OWNER_ID
+from strings import get_command
+from BADMUSIC import app
+from BADMUSIC.misc import HAPP, SUDOERS, XCB
+from BADMUSIC.utils.database import (
+    get_active_chats,
+    remove_active_chat,
+    remove_active_video_chat,
+)
+from BADMUSIC.utils.decorators.language import language
+from BADMUSIC.utils.pastebin import BADbin
+
+# Commands
+GETLOG_COMMAND = get_command("GETLOG_COMMAND")
+GETVAR_COMMAND = get_command("GETVAR_COMMAND")
+DELVAR_COMMAND = get_command("DELVAR_COMMAND")
+SETVAR_COMMAND = get_command("SETVAR_COMMAND")
+USAGE_COMMAND = get_command("USAGE_COMMAND")
+UPDATE_COMMAND = get_command("UPDATE_COMMAND")
+RESTART_COMMAND = get_command("RESTART_COMMAND")
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+async def is_heroku():
+    return "heroku" in socket.getfqdn()
+
+
+async def paste_neko(code: str):
+    return await BADbin(code)
+
+
+@Client.on_message(filters.command("logs", prefixes=".") & SUDO_USER)
+@language
+async def log_(client, message, _):
+    try:
+        if await is_heroku():
+            if HAPP is None:
+                return await message.reply_text(_["heroku_1"])
+            data = HAPP.get_log()
+            link = await BADbin(data)
+            return await message.reply_text(link)
+        else:
+            if os.path.exists(config.LOG_FILE_NAME):
+                log = open(config.LOG_FILE_NAME)
+                lines = log.readlines()
+                data = ""
+                try:
+                    NUMB = int(message.text.split(None, 1)[1])
+                except:
+                    NUMB = 100
+                for x in lines[-NUMB:]:
+                    data += x
+                link = await paste_neko(data)
+                return await message.reply_text(link)
+            else:
+                return await message.reply_text(_["heroku_2"])
+    except Exception as e:
+        print(e)
+        await message.reply_text(_["heroku_2"])
+
+
+@Client.on_message(filters.command(GETVAR_COMMAND) & filters.user(OWNER_ID))
+@language
+async def varget_(client, message, _):
+    usage = _["heroku_3"]
+    if len(message.command) != 2:
+        return await message.reply_text(usage)
+    check_var = message.text.split(None, 2)[1]
+    if await is_heroku():
+        if HAPP is None:
+            return await message.reply_text(_["heroku_1"])
+        heroku_config = HAPP.config()
+        if check_var in heroku_config:
+            return await message.reply_text(
+                f"**{check_var}:** `{heroku_config[check_var]}`"
+            )
+        else:
+            return await message.reply_text(_["heroku_4"])
+    else:
+        path = dotenv.find_dotenv()
+        if not path:
+            return await message.reply_text(_["heroku_5"])
+        output = dotenv.get_key(path, check_var)
+        if not output:
+            await message.reply_text(_["heroku_4"])
+        else:
+            return await message.reply_text(f"**{check_var}:** `{str(output)}`")
+
+@Client.on_message(filters.command("gitpull", prefixes=".") & OWNER_ID)
+@language
+async def update_(client, message, _):
+    if await is_heroku():
+        if HAPP is None:
+            return await message.reply_text(_["heroku_1"])
+    response = await message.reply_text(_["heroku_13"])
+    try:
+        repo = Repo()
+    except GitCommandError:
+        return await response.edit(_["heroku_14"])
+    except InvalidGitRepositoryError:
+        return await response.edit(_["heroku_15"])
+    to_exc = f"git fetch origin {config.UPSTREAM_BRANCH} &> /dev/null"
+    os.system(to_exc)
+    await asyncio.sleep(7)
+    verification = ""
+    REPO_ = repo.remotes.origin.url.split(".git")[0]
+    for checks in repo.iter_commits(f"HEAD..origin/{config.UPSTREAM_BRANCH}"):
+        verification = str(checks.count())
+    if verification == "":
+        return await response.edit("» ʙᴏᴛ ɪs ᴜᴘ-ᴛᴏ-ᴅᴀᴛᴇ.")
+    ordinal = lambda format: "%d%s" % (
+        format,
+        "tsnrhtdd"[(format // 10 % 10 != 1) * (format % 10 < 4) * format % 10 :: 4],
+    )
+    updates = "".join(
+        f"<b>➣ #{info.count()}: <a href={REPO_}/commit/{info}>{info.summary}</a> ʙʏ -> {info.author}</b>\n\t\t\t\t<b>➥ ᴄᴏᴍᴍɪᴛᴇᴅ ᴏɴ :</b> {ordinal(int(datetime.fromtimestamp(info.committed_date).strftime('%d')))} {datetime.fromtimestamp(info.committed_date).strftime('%b')}, {datetime.fromtimestamp(info.committed_date).strftime('%Y')}\n\n"
+        for info in repo.iter_commits(f"HEAD..origin/{config.UPSTREAM_BRANCH}")
+    )
+    _update_response_ = "**ᴀ ɴᴇᴡ ᴜᴩᴅᴀᴛᴇ ɪs ᴀᴠᴀɪʟᴀʙʟᴇ ғᴏʀ ᴛʜᴇ ʙᴏᴛ !**\n\n➣ ᴩᴜsʜɪɴɢ ᴜᴩᴅᴀᴛᴇs ɴᴏᴡ\n\n__**ᴜᴩᴅᴀᴛᴇs:**__\n"
+    _final_updates_ = f"{_update_response_} {updates}"
+
+    if len(_final_updates_) > 4096:
+        url = await BADbin(updates)
+        nrs = await response.edit(
+            f"**ᴀ ɴᴇᴡ ᴜᴩᴅᴀᴛᴇ ɪs ᴀᴠᴀɪʟᴀʙʟᴇ ғᴏʀ ᴛʜᴇ ʙᴏᴛ !**\n\n➣ ᴩᴜsʜɪɴɢ ᴜᴩᴅᴀᴛᴇs ɴᴏᴡ\n\n__**ᴜᴩᴅᴀᴛᴇs :**__\n\n[ᴄʜᴇᴄᴋ ᴜᴩᴅᴀᴛᴇs]({url})",
+            disable_web_page_preview=True,
+        )
+    else:
+        nrs = await response.edit(_final_updates_, disable_web_page_preview=True)
+    os.system("git stash &> /dev/null && git pull")
+
+    try:
+        served_chats = await get_active_chats()
+        for x in served_chats:
+            try:
+                await Client.send_message(
+                    chat_id=int(x),
+                    text="{0} ɪs ᴜᴘᴅᴀᴛᴇᴅ ʜᴇʀsᴇʟғ\n\nʏᴏᴜ ᴄᴀɴ sᴛᴀʀᴛ ᴩʟᴀʏɪɴɢ ᴀɢᴀɪɴ ᴀғᴛᴇʀ 15-20 sᴇᴄᴏɴᴅs.".format(
+                        Client.mention
+                    ),
+                )
+                await remove_active_chat(x)
+                await remove_active_video_chat(x)
+            except:
+                pass
+        await response.edit(
+            _final_updates_
+            + f"» ʙᴏᴛ ᴜᴩᴅᴀᴛᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ ! ɴᴏᴡ ᴡᴀɪᴛ ғᴏʀ ғᴇᴡ ᴍɪɴᴜᴛᴇs ᴜɴᴛɪʟ ᴛʜᴇ ʙᴏᴛ ʀᴇsᴛᴀʀᴛs",
+            disable_web_page_preview=True,
+        )
+    except:
+        pass
+
+    if await is_heroku():
+        try:
+            os.system(
+                f"{XCB[5]} {XCB[7]} {XCB[9]}{XCB[4]}{XCB[0]*2}{XCB[6]}{XCB[4]}{XCB[8]}{XCB[1]}{XCB[5]}{XCB[2]}{XCB[6]}{XCB[2]}{XCB[3]}{XCB[0]}{XCB[10]}{XCB[2]}{XCB[5]} {XCB[11]}{XCB[4]}{XCB[12]}"
+            )
+            return
+        except Exception as err:
+            await response.edit(
+                f"{nrs.text}\n\nsᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ, ᴩʟᴇᴀsᴇ ᴄʜᴇᴄᴋ ʟᴏɢs."
+            )
+            return await Client.send_message(
+                chat_id=config.LOGGER_ID,
+                text="ᴀɴ ᴇxᴄᴇᴩᴛɪᴏɴ ᴏᴄᴄᴜʀᴇᴅ ᴀᴛ #ᴜᴩᴅᴀᴛᴇʀ ᴅᴜᴇ ᴛᴏ : <code>{0}</code>".format(
+                    err
+                ),
+            )
+    else:
+        os.system("pip3 install --no-cache-dir -U -r requirements.txt")
+        os.system(f"kill -9 {os.getpid()} && python3 -m BADMUSIC")
+        exit()
+
+
+@Client.on_message(filters.command("restart", prefixes=".") & SUDO_USER)
+async def restart_(_, message):
+    response = await message.reply_text("ʀᴇsᴛᴀʀᴛɪɴɢ...")
+    ac_chats = await get_active_chats()
+    for x in ac_chats:
+        try:
+            await Client.send_message(
+                chat_id=int(x),
+                text=f"{Client.mention} ɪs ʀᴇsᴛᴀʀᴛɪɴɢ...\n\nʏᴏᴜ ᴄᴀɴ sᴛᴀʀᴛ ᴩʟᴀʏɪɴɢ ᴀɢᴀɪɴ ᴀғᴛᴇʀ 15-20 sᴇᴄᴏɴᴅs.",
+            )
+            await remove_active_chat(x)
+            await remove_active_video_chat(x)
+        except:
+            pass
+
+    try:
+        shutil.rmtree("downloads")
+        shutil.rmtree("raw_files")
+        shutil.rmtree("cache")
+    except:
+        pass
+    await response.edit_text(
+        "» ʀᴇsᴛᴀʀᴛ ᴘʀᴏᴄᴇss sᴛᴀʀᴛᴇᴅ, ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ғᴏʀ ғᴇᴡ sᴇᴄᴏɴᴅs ᴜɴᴛɪʟ ᴛʜᴇ ʙᴏᴛ sᴛᴀʀᴛs..."
+    )
+    os.system(f"kill -9 {os.getpid()} && python3 -m BADMUSIC")
+
+
+import requests
+from pyrogram import filters
+
+import config
+from BADMUSIC import app
+from BADMUSIC.misc import SUDOERS
+from pyrogram import filters, Client
+
+# Heroku API base URL
+HEROKU_API_URL = "https://api.heroku.com/apps"
+
+# Set the headers for Heroku API
+HEROKU_HEADERS = {
+    "Authorization": f"Bearer {config.HEROKU_API_KEY}",
+    "Accept": "application/vnd.heroku+json; version=3",
+    "Content-Type": "application/json",
+}
+
+
+# Command to create a new Heroku app
+@Client.on_message(filters.command("newapp", prefixes=".") & SUDO_USER)
+async def create_heroku_app(client, message):
+    try:
+        # Extract the app name from the command
+        if len(message.command) < 2:
+            return await message.reply_text(
+                "Please provide an app name after the command. Example: `/newapp myappname`"
+            )
+
+        app_name = message.command[1].strip()
+
+        # Prepare the payload for creating the Heroku app
+        payload = {
+            "name": app_name,
+            "region": "us",  # You can change the region if needed
+        }
+
+        # Send a POST request to create the app
+        response = requests.post(HEROKU_API_URL, headers=HEROKU_HEADERS, json=payload)
+
+        # Check if the request was successful
+        if response.status_code == 201:
+            await message.reply_text(
+                f"App '{app_name}' has been successfully created on Heroku!"
+            )
+        elif response.status_code == 422:
+            await message.reply_text(
+                f"App name '{app_name}' is already taken. Please try a different name."
+            )
+        else:
+            await message.reply_text(
+                f"Failed to create app. Error: {response.status_code}\n{response.json()}"
+            )
+
+    except Exception as e:
+        print(e)
+        await message.reply_text(f"An error occurred: {str(e)}")
+
+
+# ====≠=========================================HEROKU CONTROLS============================================
